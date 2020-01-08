3ee65dd1effa924a0d38c4267f7a4282797598a5
Danuka Ariyasingha Gamage <danuka.ariyasinghagamage@singtel.com>
2020-01-08 11:21:21 +0800

    Simple Listner, Transformer microservice application with kafka

--- http-listner-service/src/main/java/com/myapps/microservice/ControllerClass.java
+++ http-listner-service/src/main/java/com/myapps/microservice/ControllerClass.java
@@ -1,6 +1,14 @@
 package com.myapps.microservice;
 
+import javax.annotation.PostConstruct;
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,8 +20,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ControllerClass {
 
+    private static Log log = LogFactory.getLog(ControllerClass.class);
+		
 	@Autowired
-	private ServiceClass serviceClass;
+	CamelContext camelContext;
+
+	@Autowired
+	@Qualifier("KafkaRouteProducer")
+	RouteBuilder kafkaRouteProducer;
+	
+	@EndpointInject(uri = "direct:kafkaRoute")
+	ProducerTemplate kafkaProducer;
+	
+	
+	@PostConstruct
+	public void setup() {
+		try {
+			camelContext.addRoutes(kafkaRouteProducer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@RequestMapping("/")
 	public String defaultEnd() {
@@ -22,11 +49,13 @@ public class ControllerClass {
 	
 	@GetMapping("/v1/addUSer/{msisdn}")
 	public ResponseEntity<?> addNewUser(@PathVariable("msisdn") String msisdn) {
+		log.info("Add Normal User Request" + msisdn);
 		return new ResponseEntity<>("New User Created", HttpStatus.OK);
 	}
 
 	@DeleteMapping ("/v1/deleteUser/{msisdn}")
 	public ResponseEntity<?> deleteExistingUser(@PathVariable("msisdn") String msisdn) {
+		log.info("Delete Normal User Request" + msisdn);
 		return new ResponseEntity<>("Existing user Deleted", HttpStatus.OK);
 	}
 
@@ -35,12 +64,26 @@ public class ControllerClass {
 	
 	@GetMapping("/v1/kafka/addUSer/{msisdn}")
 	public ResponseEntity<?> addUserValidator(@PathVariable("msisdn") String msisdn) {
-		return new ResponseEntity<>(serviceClass.addUser(msisdn), HttpStatus.OK);
+		
+		log.info("Add new User via Kafka Request" + msisdn);
+
+		try {
+			kafkaProducer.sendBody("direct:kafkaRoute", "This request is from the user " + msisdn);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		return new ResponseEntity<>("New User Creation request delivered to backend service", HttpStatus.OK);
 	}
 
 
 	@DeleteMapping ("/v1/kafka/deleteUser/{msisdn}")
 	public ResponseEntity<?> deleteUserValidator(@PathVariable("msisdn") String msisdn) {
-		return new ResponseEntity<>(serviceClass.deleteUser(msisdn), HttpStatus.OK);
+		log.info("Delete User via Kafka Request" + msisdn);
+		try {
+			kafkaProducer.sendBody("direct:kafkaRoute", "This request is from the user " + msisdn);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		return new ResponseEntity<>("Delete User request delivered to backend service", HttpStatus.OK);
 	}
 }
--- http-listner-service/src/main/java/com/myapps/microservice/HttpListnerServiceApplication.java
+++ http-listner-service/src/main/java/com/myapps/microservice/HttpListnerServiceApplication.java
@@ -1,7 +1,12 @@
 package com.myapps.microservice;
 
+import javax.servlet.Servlet;
+
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HttpListnerServiceApplication {
